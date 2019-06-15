442d9c5908b4aa277392083aa88722f279ee18d5
gtgitdev <g.turton@hotmail.com>
2019-06-15 05:45:55 -0400

    Create Data

--- DAL.Tests/2_CreateTests/CreateDataTests.cs
+++ DAL.Tests/2_CreateTests/CreateDataTests.cs
@@ -16,16 +16,58 @@ namespace DAL.Tests.CreateTests
         [Fact]
         public void ShouldAddCategory()
         {
+            var count = Db.Categories.Count();
+            Assert.Equal(0, count);
+
+            var cat = new Category { CategoryName = "Foo" };
+            Assert.Equal(0, cat.Id);
+            Assert.Null(cat.TimeStamp);
+
+            Db.Categories.Add(cat);
+            Db.SaveChanges();
+
+            Assert.NotEqual(0, cat.Id);
+            Assert.NotNull(cat.TimeStamp);
+            Assert.Equal(8, cat.TimeStamp.Length);
+
+            Assert.Equal(count + 1, Db.Categories.Count());
+            var newCat = Db.Categories.FirstOrDefault(c => c.Id == cat.Id);
+            Assert.Equal(cat.CategoryName, newCat?.CategoryName);
+
         }
 
         [Fact]
         public void ShouldAddTwoCategories()
         {
+            var count = Db.Categories.Count();
+
+            var cat1 = new Category { CategoryName = "Foo" };
+            var cat2 = new Category { CategoryName = "Bar" };
+
+            Db.Categories.AddRange(new List<Category> { cat1, cat2 });
+            Db.SaveChanges();
+
+            Assert.Equal(count + 2, Db.Categories.Count());
+
+
         }
 
         [Fact]
         public void ShouldAddCategoryAndProducts()
         {
+            var prodCount = Db.Products.Count();
+            var cat = new Category { CategoryName = "Foo" };
+
+            cat.Products.Add(new Product { ModelName = "Foo" });
+            cat.Products.Add(new Product { ModelName = "Bar" });
+            cat.Products.Add(new Product { ModelName = "FooBar" });
+
+            Db.Categories.Add(cat);
+            Db.SaveChanges();
+
+            Assert.Equal(prodCount + 3, Db.Products.Count());
+
+
         }
 
     }
